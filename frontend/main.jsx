548fb6170530aac44be70e52cd102bfa5fdb10b0
Mr penington <benjaminbkris@gmail.com>
2026-01-27 14:45:55 +0100

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,2 +1,7 @@
 ReactDOM.createRoot(document.getElementById("root"))
   .render(<App />);
+
+function App() {
+    return <h1>Welcom to my site</h1>,
+    <h2>If you see this then I AM A FUCKING GENIUS! HIGH SCORE BABY, HIGH ON METAMFETAMIN</h2>
+}
