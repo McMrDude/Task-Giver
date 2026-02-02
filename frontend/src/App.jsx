86f294b6800790af7fd41155632285d60d14af9b
Mr penington <benjaminbkris@gmail.com>
2026-02-02 10:31:38 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
+  const [messages, setMessages] = useEffect([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/messages")
+    .then(res => res.json())
+    .then(data => setMessages(data))
+    .catch(err => console.error(err))
+  }, [])
   return(
     <>
       <h1>Welcome dipshit</h1>
       <h2>If all this text comes up then I AM TO GOOD AND I WILL BE DO BE DOING BE DO SMOKING METANFETAMIN BABY!!!</h2>
+      {messages.map(msg => (
+        <p key={msg.id}>{msg.text}</p>
+      ))}
     </>
   )
 }
