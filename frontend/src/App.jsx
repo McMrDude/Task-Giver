8f5898822e1a6d967ac621f69f85a650339b46a9
Mr penington <benjaminbkris@gmail.com>
2026-02-03 13:41:33 +0100

    r

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Register from './components/Register'
+import Register from './Register'
 
 function App() {
   const [messages, setMessages] = useState([])
