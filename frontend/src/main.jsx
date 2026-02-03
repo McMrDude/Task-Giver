b524654222306cfa2672f4ea24c4727e90c01fa2
Mr penington <benjaminbkris@gmail.com>
2026-02-03 11:21:49 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Register from './Register.jsx'
+import Register from './components/Register.jsx'
 import { Route } from 'react-router-dom'
 import { BrowserRouter, Routes } from 'react-router-dom'
 
