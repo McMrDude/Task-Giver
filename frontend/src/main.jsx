5db86f2f5f24e2aeaa80f9c3dffdf8ab639db84f
Mr penington <benjaminbkris@gmail.com>
2026-02-04 10:20:28 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Register from './components/Register.jsx'
-import Login from './components/Login.jsx'
+import Login from './components/login.jsx'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
