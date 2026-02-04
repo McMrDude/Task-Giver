8b0304b78c72c25652977ae99853fa40c983e00c
Mr penington <benjaminbkris@gmail.com>
2026-02-04 09:55:34 +0100

    e

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Register from './components/Register.jsx'
+import Login from './components/Login.jsx'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
