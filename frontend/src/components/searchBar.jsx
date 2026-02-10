d1e2b293ab363bff8de403dec755bbe21e9939ba
Mr penington <benjaminbkris@gmail.com>
2026-02-10 13:44:24 +0100

    Update searchBar.jsx

--- frontend/src/components/searchBar.jsx
+++ frontend/src/components/searchBar.jsx
@@ -23,12 +23,7 @@ function UserSearch({ users }) {
 
   return (
     <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column"
-      }}
+      
     >
       <input
         ref={wrapperRef}
