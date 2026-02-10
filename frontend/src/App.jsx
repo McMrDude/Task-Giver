8182dff09c270ed29fb9002242a49010496548db
Mr penington <benjaminbkris@gmail.com>
2026-02-10 09:26:42 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,14 @@ import './App.css'
 import UserSearch from './components/searchBar.jsx'; 
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error(err));
+  }, []);
 
   const [query, setQuery] = useState(""); // what the user types
 
@@ -16,6 +21,8 @@ function App() {
   const filteredUsers = users.filter(user =>
     user.name.toLowerCase().includes(query.toLowerCase())
   );
+
+  const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
   fetch("/api/me", { credentials: "include" })
