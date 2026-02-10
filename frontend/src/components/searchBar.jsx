7caf4b17b40388c5aa7202534c1940163bb97e27
Mr penington <benjaminbkris@gmail.com>
2026-02-10 13:51:39 +0100

    e

--- frontend/src/components/searchBar.jsx
+++ frontend/src/components/searchBar.jsx
@@ -23,7 +23,12 @@ function UserSearch({ users }) {
 
   return (
     <div
-      
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column"
+      }}
     >
       <input
         ref={wrapperRef}
