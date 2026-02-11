1ae4bf3fcfac89493ed6a3d7b685f6617c687e5b
Mr penington <benjaminbkris@gmail.com>
2026-02-11 14:27:24 +0100

    Update searchBar.jsx

--- frontend/src/components/searchBar.jsx
+++ frontend/src/components/searchBar.jsx
@@ -23,46 +23,55 @@ function UserSearch({ users }) {
 
   return (
     <div
-      ref={wrapperRef}
       style={{
-        width: "292px",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         flexDirection: "column"
       }}
     >
-      <input
-        
-        type="text"
-        placeholder="Search users..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onFocus={() => setOpen(true)} // 👈 show list when input is focused
-        style={{ width: "200px", padding: "5px" }}
-      />
+      <div
+        ref={wrapperRef}
+        style={{
+          width: "292px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column"
+        }}
+      >
+        <input
+          
+          type="text"
+          placeholder="Search users..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onFocus={() => setOpen(true)} // 👈 show list when input is focused
+          style={{ width: "200px", padding: "5px" }}
+        />
 
-      {open && (
-        <div
-          id="searchDiv"
-          className="backWrapSearch"
-        >
-          <div style={{ }}>
-            {filteredUsers.length > 0 ? (
-              filteredUsers.map(user => (
-                <h4
-                  key={user.id}
-                  style={{ border: "2px solid #5f5f5f", backgroundColor: "#444444" }}
-                >
-                  {user.name} ({user.email})
-                </h4>
-              ))
-            ) : (
-              <p style={{ color: "#aaa" }}>No users found</p>
-            )}
+        {open && (
+          <div
+            id="searchDiv"
+            className="backWrapSearch"
+          >
+            <div style={{ }}>
+              {filteredUsers.length > 0 ? (
+                filteredUsers.map(user => (
+                  <h4
+                    key={user.id}
+                    style={{ border: "2px solid #5f5f5f", backgroundColor: "#444444" }}
+                  >
+                    {user.name} ({user.email})
+                  </h4>
+                ))
+              ) : (
+                <p style={{ color: "#aaa" }}>No users found</p>
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
